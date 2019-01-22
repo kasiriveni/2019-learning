bdc7bcb94d2b5c04508b0e61ac6803757258fc3c
kasiriveni <kasiriveni@gmail.com>
2019-01-22 19:05:56 +0530

    updated

--- jan/learn-mobx/src/component/TimerView.js
+++ jan/learn-mobx/src/component/TimerView.js
@@ -4,13 +4,11 @@ import { observer } from "mobx-react";
 
 export class AppState {
     @observable timer = 0;
-
     constructor() {
         setInterval(() => {
             this.timer += 1;
         }, 1000);
     }
-
     @action.bound
     reset() {
         this.timer = 0;
